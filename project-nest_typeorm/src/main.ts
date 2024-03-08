1846f194406de17bbd45d72c07299a36945e52d0
Shubham-wappnet <shubham.wappnet@gmail.com>
2024-03-08 18:16:17 +0530

    08/03 changes-added

--- project-nest_typeorm/src/main.ts
+++ project-nest_typeorm/src/main.ts
@@ -1,8 +1,19 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TypeOrm Module')
+    .setDescription('The user-photo API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
