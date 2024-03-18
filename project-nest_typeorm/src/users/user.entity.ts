c11fc181d885316f830918e36e09858cb6199dff
Shubham-wappnet <shubham.wappnet@gmail.com>
2024-03-18 19:02:20 +0530

    19/02

--- project-nest_typeorm/src/users/user.entity.ts
+++ project-nest_typeorm/src/users/user.entity.ts
@@ -20,6 +20,9 @@ export class User {
   @Column({ default: true })
   isActive: boolean;
 
+  @Column()
+  password:string;
+
   @OneToMany(()=>Photo, photo=>photo.user)
   photos:Photo[];
 }
--- project-nest_typeorm/src/users/user.service.ts
+++ project-nest_typeorm/src/users/user.service.ts
@@ -2,15 +2,13 @@
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { User } from "./user.entity";
-import { Photo } from "src/photos/photo.entity";
+// import { JwtService } from "@nestjs/jwt";
+// import * as bcrypt from 'bcryptjs'
 
 
-export class UserService{
-    constructor(@InjectRepository(User) private userRepository:Repository<User>,
-                @InjectRepository(Photo) private photoRepository:Repository<Photo>
-                ){}
+export class UserService {
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) { }
 
-    
     async addAll(user: User): Promise<User> {
         const { email,firstName,lastName,isActive } = user;
     
@@ -20,25 +18,56 @@ export class UserService{
     
         return await this.userRepository.save(newUser);
     }
+    // async signup(user: User): Promise<User> {
+    //     const { email, firstName, lastName, isActive, password } = user;
+    //     const existingUser = await this.userRepository.findOneBy({ email })
+    //     if (!existingUser) {
+    //         console.log("User is already exist")
+    //     }
+    //     const hashPassword = await bcrypt.hash(password, 10)
+
+    //     const newUser = this.userRepository.create({
+    //         email, firstName, lastName, isActive, password: hashPassword
+    //     });
+    //     return await this.userRepository.save(newUser);
+
+    // }
+
+    // async login(user: User): Promise<{ accessToken: string }> {
+    //     const { email, password } = user;
+    //     const validUser = await this.userRepository.findOneBy({ email });
+    //     if (!validUser) {
+    //         console.log("user not found")
+    //     }
+    //     const isPasswordValid=await bcrypt.compare(password,user.password)
+    //     if(!isPasswordValid){
+    //         console.log("password not match")
+    //     }
+    //     const payload={email:user.email}
+    //     const accessToken=this.jwtService.sign(payload)
+    //     return {accessToken}
+    // }
+
+
 
     findAll(): Promise<User[]> {
         return this.userRepository.find();
     }
 
-    findOne(id:number):Promise<User>{
-        return this.userRepository.findOneBy({id})
+    findOne(id: number): Promise<User> {
+        return this.userRepository.findOneBy({ id })
     }
 
     async findPhotosByUserId(id: number): Promise<User> {
-        const photo = await this.userRepository.findOne({where:{id},relations:["photos"]});
+        const photo = await this.userRepository.findOne({ where: { id }, relations: ["photos"] });
         if (!photo) {
-          throw new Error('photo not found');
+            throw new Error('photo not found');
         }
         return photo;
-    }    
+    }
 
-    async remove(id:number):Promise<any>{
-        await this.userRepository.delete({id})
+    async remove(id: number): Promise<any> {
+        await this.userRepository.delete({ id })
     }
 }
 
